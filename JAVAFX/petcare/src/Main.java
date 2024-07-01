9a3e34417bd4d91e9a73bc480cfd18df9cda4a4d
Jadhavamar <Jadhavamar52001@gmail.com>
2024-07-01 16:02:01 +0530

    amar

---
+++ JAVAFX/petcare/src/Main.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import view.pethome;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        Application.launch(pethome.class, args);
+
+    }
+}
---
+++ JAVAFX/petcare/src/view/dogfoodh.java
@@ -0,0 +1,17 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class dogfoodh extends Application{
+
+    @Override
+    public void start(Stage primaryStage)  {
+        
+        primaryStage.setTitle("Pet Shop UI");
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
+    }
+    
+}
